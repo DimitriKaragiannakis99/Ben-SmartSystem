95e1d605039649399c85d08696611e7e03e6f8cc
benhitterman <benhitterman7@gmail.com>
2024-03-24 17:04:21 -0400

    separated simulation time and timer for SHH

--- backend/src/main/java/com/bensmartsystem/backend/controller/TimeController.java
+++ backend/src/main/java/com/bensmartsystem/backend/controller/TimeController.java
@@ -1,4 +1,5 @@
 package com.bensmartsystem.backend.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.bensmartsystem.backend.model.Time;
 @RestController
@@ -6,24 +7,27 @@ import com.bensmartsystem.backend.model.Time;
 @RequestMapping("/api/time")
 public class TimeController 
 {
-    private Time currentTime = new Time();
+    private Time currentTime;
 
-    public TimeController() {
-
+    @Autowired
+    public TimeController(Time currentTime) {
+        // Hardcoded time data
+        currentTime.setHour(12);
+        currentTime.setDay(1);
+        currentTime.setMonth(1);
+        this.currentTime = currentTime;
     }
 
     @PostMapping("/setTime")
     public void setTime(@RequestBody Time time) {
-        //Print to the console the new user added
-        System.out.println("New Time set" + time.getHour() + " " + time.getMinute() + " " + time.getSecond());
+        //Print to the console the newuser added
+        System.out.println("New Time set" + time.getHour() + " " + time.getDay() + " " + time.getMonth());
         currentTime = time;
-        currentTime.tick();
         SimulationEventManager.getInstance().Notify("timeChanged");
     }
 
     @GetMapping("/getTime")
     public Time getTime() {
-        currentTime.tick();
         return this.currentTime;
     }
     
