f30b30f2ae30984f0313cb5952c9cfadf106eb68
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2024-04-07 11:07:05 -0400

    Finished the front end for closing all doors and windows

--- frontend/src/Components/Dashboard/DashboardSHP.js
+++ frontend/src/Components/Dashboard/DashboardSHP.js
@@ -5,7 +5,7 @@ import { RoomContext } from "./RoomProvider";
 const DashboardSHP = () => {
     const [rooms, setRooms] = useState([]);
     const [isAwayModeOn, setIsAwayModeOn] = useState(false);
-    const { toggleHasMotionDetector, toggleMotionDetector } = useContext(RoomContext);
+    const { toggleHasMotionDetector, toggleMotionDetector, setAllWindowsAndDoorsOff } = useContext(RoomContext);
     const [timerDelay, setTimerDelay] = useState(30);
 
     useEffect(() => {
@@ -63,7 +63,7 @@ const DashboardSHP = () => {
                     setIsAwayModeOn(response.data.isAwayModeOn);
                     console.log('Away mode toggled. Current state:', response.data.isAwayModeOn);
                     //Update the simulation screen
-
+                    setAllWindowsAndDoorsOff();
                     
                 } else {
                     console.error('Unexpected response structure:', response.data);
